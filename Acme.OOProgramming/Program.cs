99fd6e206550975b0787ec5cb0688d89016472ed
Efrain Bautista <efrain@thibau.io>
2026-09-02 09:11:44 -0500

    feat(main): register the initial supplier.

--- Acme.OOProgramming/Program.cs
+++ Acme.OOProgramming/Program.cs
@@ -1,1 +1,8 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Acme.OOProgramming.SupplyChain.Domain.Model.Aggregates;
+using Acme.OOProgramming.Shared.Domain.Model.ValueObjects;
+using Acme.OOProgramming.SupplyChain.Domain.Model.ValueObjects;
+
+var supplierAddress = new Address("Supplier St", "123", "SupplierCity", null, "12345", "United States");
+var supplier = new Supplier(new SupplierId("SUP001"), "Supplier Inc.", supplierAddress);
+
+Console.WriteLine($"Registered Supplier {supplier.Id.Identifier}: {supplier}");
